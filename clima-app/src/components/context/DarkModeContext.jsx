89e6d7ed55e837a3fa14c7f0a2ec5809f9fbdde7
Aaronnow-Eqr <79377629+Aaronnow-Eqr@users.noreply.github.com>
2025-10-30 19:30:33 -0400

    logica del modo oscuro

---
+++ clima-app/src/components/context/DarkModeContext.jsx
@@ -0,0 +1,6 @@
+import { createContext, useState, useEffect } from 'react';
+
+export const DarkModeContext = createContext();
+
+export const DarkModeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
