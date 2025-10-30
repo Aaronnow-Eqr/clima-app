b9bd28abfe104413304c73661cf74b15c3ac21ae
Aaronnow-Eqr <79377629+Aaronnow-Eqr@users.noreply.github.com>
2025-10-30 19:38:34 -0400

    Update main.jsx

--- clima-app/src/main.jsx
+++ clima-app/src/main.jsx
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Home from "./components/pages/home.jsx";
-
+import { DarkModeProvider } from "./components/context/DarkModeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <DarkModeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </DarkModeProvider>
   </React.StrictMode>
 );
